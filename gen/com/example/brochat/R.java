bc9b2c60fd408939f4106feb7cb86f9a1a4de8cc
ABhimsaria <abhi.oficial@gmail.com>
2016-03-08 10:00:04 +0530

    Added a camera Buton

--- gen/com/example/brochat/R.java
+++ gen/com/example/brochat/R.java
@@ -23,13 +23,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_photo_camera_black_24dp=0x7f020001;
     }
     public static final class id {
+        public static final int action_camera=0x7f08000a;
         public static final int action_edit_friends=0x7f08000b;
         public static final int action_settings=0x7f080009;
         public static final int emailField=0x7f080007;
         public static final int loginButton=0x7f080002;
-        public static final int logout_settings=0x7f08000a;
+        public static final int logout_settings=0x7f08000c;
         public static final int pager=0x7f080004;
         public static final int passwordField=0x7f080001;
         public static final int passwordField1=0x7f080006;
@@ -55,19 +57,20 @@ public final class R {
         public static final int action_settings=0x7f050005;
         public static final int app_name=0x7f050000;
         public static final int email_hint=0x7f05000d;
-        public static final int empty_friends_label=0x7f05000f;
-        public static final int empty_inbox_label=0x7f05000e;
+        public static final int empty_friends_label=0x7f050010;
+        public static final int empty_inbox_label=0x7f05000f;
         public static final int hello_world=0x7f050004;
         public static final int login_button_label=0x7f050009;
-        public static final int login_error_message=0x7f050012;
+        public static final int login_error_message=0x7f050013;
+        public static final int menu_camera_label=0x7f05000e;
         public static final int menu_edit_friends_label=0x7f05000b;
-        public static final int menu_logout_label=0x7f050013;
+        public static final int menu_logout_label=0x7f050014;
         public static final int password_hint=0x7f050008;
-        public static final int sign_up_button_label=0x7f050010;
-        public static final int sign_up_error_title=0x7f050014;
+        public static final int sign_up_button_label=0x7f050011;
+        public static final int sign_up_error_title=0x7f050015;
         public static final int sign_up_text=0x7f05000a;
-        public static final int signup_error_message=0x7f050011;
-        public static final int title_activity_edit_friends=0x7f050015;
+        public static final int signup_error_message=0x7f050012;
+        public static final int title_activity_edit_friends=0x7f050016;
         public static final int title_activity_login=0x7f050006;
         public static final int title_activity_sign_up=0x7f05000c;
         public static final int title_section1=0x7f050001;
